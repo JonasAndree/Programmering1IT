b012f4c0cda05ddee30aa3b22d26bb906ab749bc
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-08-29 11:16:45 +0200

    min bil update

--- src/lektion2/Bil.java
+++ src/lektion2/Bil.java
@@ -1,9 +1,13 @@
 package lektion2;
 
 public class Bil {
-
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		int ANTALET_RATTAR = 4;
+		int ANTALET_RATTAR2 = 2;
 
 		System.out.println(ANTALET_RATTAR);
 
