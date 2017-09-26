f6c3bd98288e491ac39dcd19e1546f49a8de0cc9
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-09-26 09:59:44 +0200

    min update

--- src/classes/exercises1/Application.java
+++ src/classes/exercises1/Application.java
@@ -2,9 +2,8 @@ package classes.exercises1;
 
 public class Application {
 	public static void main(String[] args) {
-        
-        System.out.println("Hello World!");
-        
-}
-}
 
+		System.out.println("Hello World!");
+
+	}
+}
