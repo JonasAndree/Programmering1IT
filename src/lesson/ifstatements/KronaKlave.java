4483a7c40aa5777c1bf5825780966ea3375f07ed
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-10-24 10:30:52 +0200

    Krona klave

---
+++ src/lesson/ifstatements/KronaKlave.java
@@ -0,0 +1,41 @@
+package lesson.ifstatements;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class KronaKlave {
+
+	public static void main(String[] args) {
+		// Users scanner input
+		Scanner usersGuess = new Scanner(System.in);
+		System.out.println("Select Heads or Tails: ");
+		String usersPick = usersGuess.nextLine().toLowerCase();
+		
+	//	System.out.println(usersPick);
+		
+		
+		// Computers random selector.
+		Random randomGenerator = new Random();
+		int coinFlippValue = randomGenerator.nextInt(3);
+		
+		
+	//	System.out.println(coinFlippValue);
+		
+		
+		if (coinFlippValue == true) {
+			System.out.println("Computer - Heads");
+		} else {
+			System.out.println("Computer - Tails");
+		}
+		
+		
+		if (coinFlippValue == true && usersPick.equals("heads") || coinFlippValue == false && usersPick.equals("tails")) {
+			System.out.println("You won!");
+		}  else {
+			System.out.println("You lost!!!!");
+		}
+		
+		
+		
+	}
+}
