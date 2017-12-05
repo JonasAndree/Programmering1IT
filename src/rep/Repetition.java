0ce5f669d40035f4a6730c34f35e6ccb53cc24e4
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-12-05 10:56:53 +0100

    Intro Game strukture

--- src/rep/Repetition.java
+++ src/rep/Repetition.java
@@ -11,6 +11,9 @@ public class Repetition {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		functionNoReturn();
+		
+		
 		// Varibaler innehåller variabeltyp och variabel namn 
 		int heltal = 15;
 		float decimaltal = 1235.12f;
@@ -27,7 +30,7 @@ public class Repetition {
 		//String inputFromUser = scanner.nextLine();
 		//System.out.println("User input: " + inputFromUser);
 		
-	/*	
+		/*	
 		System.out.println((heltal + 15) / 3);
 		System.out.println("Hej" + (15 + 15));
 		
@@ -41,28 +44,36 @@ public class Repetition {
 		JOptionPane.showMessageDialog(null, userInputFromOptionPane);
 		
 		*/
-		while (true) {
+		
+		boolean loopOnOff = true;
+		
+		while (loopOnOff) {
 			String userInputFromOptionPane1 = JOptionPane.showInputDialog(null, "Hur många kg kött äter du?");
 		/*String userInputFromOptionPane2 = JOptionPane.showInputDialog("message2", "initialSelectionValue");
 		String userInputFromOptionPane3 = JOptionPane.showInputDialog(null, "message3", "initialSelectionValue2");
 		String userInputFromOptionPane4 = JOptionPane.showInputDialog(null, "message4", "title1", -1);
 		JOptionPane.showInputDialog(null, "message5", "title2", 3, testIcon, null, null);
 		*/
-		
 			int integerInput = Integer.parseInt(userInputFromOptionPane1);
 			
-			if (integerInput >= 35) {
+			if (integerInput > 35) {
 				JOptionPane.showMessageDialog(null, "Oj vad mycket!");
 			} else if (integerInput >= 10) {
 				JOptionPane.showMessageDialog(null, "Ganska bra!");
 			} else {
 				JOptionPane.showMessageDialog(null, "Vad bra!");
-				break;
+				loopOnOff = false;
 			}
-			
-		}
-		
-		
+		}	
 	}
-
+	
+	
+	public static void functionNoReturn() {
+		JOptionPane.showMessageDialog(null, "Hej!");
+	}
+	
+	
+	
+	
+	
 }
